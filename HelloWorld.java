0231b1a34e56e16fee803a8db28a86573277da13
chrisyoungclaus <chris.youngclaus@constantcontact.com>
2025-08-14 10:23:34 -0400

    update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,19 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        java.time.DayOfWeek today = java.time.LocalDate.now().getDayOfWeek();
+        switch (today) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+            case FRIDAY:
+                System.out.println("Hello World");
+                break;
+            case SATURDAY:
+            case SUNDAY:
+                System.out.println("I am sleeping");
+                break;
+        }
     }
 }
